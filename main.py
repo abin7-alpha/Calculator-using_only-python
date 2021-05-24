1e9e41c37742d25d67774bbfdadffb1b7c274506
abin paul <paulabin9@gmail.com>
2021-05-24 14:27:48 +0530

    modified

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from tkinter import *
+from tkinter import Entry, Button, Tk, RIDGE, END
 
-ws = Tk()
-ws.title("calculator")
+calculator = Tk()
+calculator.title("calculator")
 
-info_Tf = Entry(ws, bd=30, insertwidth=4, relief=RIDGE, bg="peach puff")
-info_Tf.insert(END, '')
-info_Tf.grid(columnspan=5)
+info_Tf = Entry(calculator, bd=30, insertwidth=4, relief=RIDGE, bg="peach puff")
+info_Tf.grid(columnspan=10)
 
 def add():
     info_Tf.insert(END, '+')
@@ -43,28 +42,40 @@ def changeback():
     info_Tf.delete(0, END)  
 
 def calculate():
-    current_values = info_Tf.get()
-    summation = str(eval(current_values))
-    info_Tf.delete(0, END)
-    info_Tf.insert(END, summation)
+    expression = info_Tf.get()
+    if not expression == '':
+        result = eval(expression)
+        info_Tf.delete(0, END)
+        info_Tf.insert(END, result)
+    else:
+        info_Tf.insert(END, 'ERROR')
+        print('Empty Expression!')
     
-b1 = Button(ws, bg="peach puff", padx=20, pady=20,fg="black", text='1', justify= "left", command= one).grid(row=1, column=0)
-b2 = Button(ws, bg="peach puff", padx=20, pady=20,fg="black", text='2', command=two).grid(row=1, column=2)
-b3 = Button(ws, bg="peach puff", padx=20, pady=20,fg="black", text='3', command=three).grid(row=1, column=3)
-adds = Button(ws, bg="peach puff", padx=20, pady=20,fg="black", text='+', command=add).grid(row=1, column=4)
-b4 = Button(ws, bg="peach puff", padx=20, pady=20,fg="black", text='4', justify= "left", command= four).grid(row=2, column=0)
-b5 = Button(ws, bg="peach puff", padx=20, pady=20,fg="black", text='5', command=five).grid(row=2, column=2)
-b6 = Button(ws, bg="peach puff", padx=20, pady=20,fg="black", text='6', command=six).grid(row=2, column=3)
-subs = Button(ws, bg="peach puff", padx=20, pady=20,fg="black", text='-', command=sub).grid(row=2, column=4)
-b7 = Button(ws, bg="peach puff", padx=20, pady=20,fg="black", text='7', justify= "left", command= seven).grid(row=3, column=0)
-b8 = Button(ws, bg="peach puff", padx=20, pady=20,fg="black", text='8', command=eight).grid(row=3, column=2)
-b9 = Button(ws, bg="peach puff", padx=20, pady=20,fg="black", text='9', command=nine).grid(row=3, column=3)
-multi = Button(ws, bg="peach puff", padx=20, pady=20,fg="black", text='*', command=mul).grid(row=3, column=4)
-decm = Button(ws, bg="peach puff", padx=20, pady=20,fg="black", text='.', justify= "left", command= decimal).grid(row=4, column=0)
-zer = Button(ws, bg="peach puff", padx=20, pady=20,fg="black", text='0', command=zero).grid(row=4, column=2)
-divs = Button(ws, bg="peach puff", padx=20, pady=20,fg="black", text='/', command=div).grid(row=4, column=3)
-cnbc = Button(ws, bg="peach puff", padx=20, pady=20,fg="black", text='c', command=changeback).grid(row=4, column=4)
-equalto = Button(ws, bg="peach puff", padx=105, pady=20,text='=', command=calculate).grid(columnspan=5)
+buttons = {
+    '1': (one, 1, 0),
+    '2': (two, 1, 2),
+    '3': (three, 1, 3),
+    '+': (add, 1, 4),
+    '4': (four, 2, 0),
+    '5': (five, 2, 2),
+    '6': (six, 2, 3),
+    '-': (sub, 2, 4),
+    '7': (seven, 3, 0),
+    '8': (eight, 3, 2),
+    '9': (nine, 3, 3),
+    '*': (mul, 3, 4),
+    '.': (decimal, 4, 0),
+    '0': (zero, 4, 2),
+    '/': (div, 4, 3),
+    'C': (changeback, 4, 4)
+
+}
+
+for key, value in buttons.items():
+    action, row, col = value #(one, 1, 0)
+    Button(calculator, bg="peach puff", padx=20, pady=20,fg="black", text=key, command=action).grid(row=row, column=col)
+
+equalto = Button(calculator, bg="peach puff", padx=105, pady=20,text='=', command=calculate).grid(columnspan=5)
 
 if __name__ == '__main__':
-    ws.mainloop()
+    calculator.mainloop()
